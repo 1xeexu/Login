12865e4d1fe4f3559f2476835dc1b48431bf30e6
Juyeong <1976zoq@gmail.com>
2025-08-04 15:43:45 +0900

    fix: alert 전 border 스타일이 적용되도록 setTimeout 추가

--- script.js
+++ script.js
@@ -5,27 +5,34 @@ const signUpButton = document.querySelector(".button-signup");
 let userId;
 let userPw;
 
-function validateInput(inputId, inputPw) {
+function showAlert(message) {
+    setTimeout(() => alert(message), 10);
+}
+
+function initStyle() {
     id.style.border = "";
     password.style.border = "";
+}
+
+function validateInput(inputId, inputPw) {
+    initStyle();
 
     if (!inputId) {
-        //alert("아이디를 입력하세요.");
         id.style.border = "2px solid red";
         return false;
     }
     else if (!inputPw) {
-        //alert("비밀번호를 입력하세요.");
         password.style.border = "2px solid red";
         return false;
     }
-    else if (inputId.length < 6) {
-        alert("아이디는 6자 이상이어야 합니다.");
+    else if (inputId.length < 6 || inputId.length > 12) {
+        id.style.border = "2px solid red";
+        showAlert("아이디는 6자 이상 12자 이내여야 합니다.");
         return false;
     }
     else if (inputPw.length < 8 || inputPw.length > 12) {
-        alert("비밀번호는 8자 이상 12자 이내여야 합니다.");
-        password.value = "";
+        password.style.border = "2px solid red";
+        showAlert("비밀번호는 8자 이상 12자 이내여야 합니다.");
         return false;
     }
     return true;
@@ -36,14 +43,18 @@ const handleLogin = function() {
     const inputPw = password.value;
 
     if (validateInput(inputId, inputPw)) {
-        if (inputId == userId && inputPw == userPw) {
-            alert("로그인에 성공하였습니다.");
-            id.value = "";
-            password.value = "";
-        }
-        else {
-            alert("회원 정보가 없습니다.");
-        }
+        initStyle();
+
+        setTimeout(() => {
+            if (inputId == userId && inputPw == userPw) {
+                alert("로그인에 성공하였습니다.");
+                id.value = "";
+                password.value = "";
+            }
+            else {
+                alert("회원 정보가 없습니다.");
+            }
+        }, 10);
     }
 };
 
@@ -52,13 +63,18 @@ const handleSignUp = function() {
     const inputPw = password.value;
 
     if (validateInput(inputId, inputPw)) {
-        alert("회원가입에 성공하였습니다.");
+        initStyle();
 
-        userId = inputId;
-        userPw = inputPw;
+        setTimeout(() => {
+            alert("회원가입에 성공하였습니다.");
 
-        id.value = "";
-        password.value = "";
+            userId = inputId;
+            userPw = inputPw;
+
+            id.value = "";
+            password.value = "";
+        }, 10);
+
     }
 };
 
